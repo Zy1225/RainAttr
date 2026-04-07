aead6d3f4cd208096d5a155675ae4c8e94209bfc
Zhi Yang Tho <u5738531@anu.edu.au>
2026-04-07 17:49:33 +1000

    Updated vignettes to show datasets included with the package
    Updated documentation of gaugeday_downwind dataset

--- R/gaugeday_downwind.R
+++ R/gaugeday_downwind.R
@@ -5,7 +5,7 @@
 #' This dataset contains binary indicators of whether each gauge-day observation is downwind of each ionizer during the 2013--2018 Oman rainfall enhancement trial, as used in the analyses of Chambers et al. (2022a,b).
 #'
 #'
-#' @format A binary matrix with 122259 rows (gauge-days) and 10 variables:
+#' @format A binary matrix with 122259 rows (corresponding to gauge-days in \code{\link{oman}} dataset) and 10 variables:
 #' \describe{
 #'    \item{H1 -- H10}{Binary indicators for each ionizer, showing whether a gauge-day observation is downwind of that ionizer: 1 = downwind; 0 = not downwind; NA = ionizer not yet deployed}
 #'}
